06ea87887bbe4c72e74742ea92050ba37d855df9
Najlase <seghaier.najla95@gmail.com>
2021-02-19 11:19:15 +0100

    edit profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -14,18 +14,48 @@ export class ProfileComponent implements OnInit {
   email: string = "";
   password: string = "";
   confirmedPassword: string = "";
+  profilePicture: string = "";
+  showPictureUpload: boolean = false;
+  showPictureLink: boolean = false;
+  error: string = " ";
+  success: string = " ";
 
-  constructor(public authService: AuthService, private profileService: ProfileService) { }
+  constructor(public authService: AuthService, private profileService: ProfileService) {
+    if (authService.user) {
+      this.firstName = authService.user.profile.firstName;
+      this.lastName = authService.user.profile.lastName;
+      this.email = authService.user.email;
+      this.profilePicture = authService.user.profile.img;
+    }
+  }
 
   ngOnInit(): void {
   }
 
   saveProfile(): void {
+    this.error = " ";
     if (this.password == this.confirmedPassword)
-      this.profileService.updateProfile({firstName: this.firstName, lastName: this.lastName, email: this.email, password: this.password})
-      .then(res => this.authService.user = res)
+      this.profileService.updateProfile({ firstName: this.firstName, lastName: this.lastName, email: this.email, password: this.password, profilePicture: this.profilePicture })
+        .then(res => {
+          this.authService.user = res
+          this.success = "Profile updated sucessfully"
+          setTimeout(()=>this.success = " ", 3000)
+        })
     else
-      console.log("error");
+      this.error = "passwords don't match, please reenter password";
+  }
+
+  handleFileInput(event: any) {
+    let self = this;
+    let reader = new FileReader();
+    reader.readAsDataURL(event.target?.files[0]);
+    reader.onload = function () {
+      console.log(reader.result);
+      self.profilePicture = reader.result?.toString() ?? "";
+    };
+    reader.onerror = function (error) {
+      console.log('Error: ', error);
+    };
   }
 
 }
